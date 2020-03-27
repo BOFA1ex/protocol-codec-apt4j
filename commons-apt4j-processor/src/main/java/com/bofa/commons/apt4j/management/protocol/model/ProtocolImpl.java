54044ea54bed6c4840297db202abbce2839f4220
BOFA1ex <BOFA1ex@163.com>
2020-03-28 01:10:56 +0800

    移除ByteBufConvertAnonModel 直接引用ByteBufConvert 移除InitResolveException 移除ProtoclSpelProcessExpr 直接引用ByteBufConvert

--- commons-apt4j-processor/src/main/java/com/bofa/commons/apt4j/management/protocol/model/ProtocolImpl.java
+++ commons-apt4j-processor/src/main/java/com/bofa/commons/apt4j/management/protocol/model/ProtocolImpl.java
@@ -2,14 +2,16 @@ package com.bofa.commons.apt4j.management.protocol.model;
 
 import com.bofa.commons.apt4j.management.internal.model.TypeHeadModel;
 import com.bofa.commons.apt4j.management.internal.writable.JavaModelWritable;
-import com.bofa.commons.apt4j.management.protocol.model.decode.ProtocolDecode$0;
-import com.bofa.commons.apt4j.management.protocol.model.decode.ProtocolDecode$1;
-import com.bofa.commons.apt4j.management.protocol.model.encode.ProtocolEncode$0;
-import com.bofa.commons.apt4j.management.protocol.model.encode.ProtocolEncode$1;
+import com.bofa.commons.apt4j.management.protocol.model.decode.ProtocolOverrideDecodeMethod;
+import com.bofa.commons.apt4j.management.protocol.model.decode.ProtocolDecodeInternalMethod;
+import com.bofa.commons.apt4j.management.protocol.model.encode.ProtocolOverrideEncodeMethod;
+import com.bofa.commons.apt4j.management.protocol.model.encode.ProtocolEncodeInternalMethod;
 import com.google.common.base.Strings;
 import lombok.*;
 
 import java.util.*;
+
+import static com.bofa.commons.apt4j.management.protocol.constant.ProtocolGenerateConstant.CHANNEL_UTILS_QUALIFIER_NAME;
 
 
 /*
@@ -25,10 +27,10 @@ public class ProtocolImpl extends JavaModelWritable {
 
     private String package_name;
     private TypeHeadModel type_head;
-    private ProtocolDecode$0 decode_root_element;
-    private List<ProtocolDecode$1> decode_elements;
-    private ProtocolEncode$0 encode_root_element;
-    private List<ProtocolEncode$1> encode_elements;
+    private ProtocolOverrideDecodeMethod decode_root_element;
+    private List<ProtocolDecodeInternalMethod> decode_elements;
+    private ProtocolOverrideEncodeMethod encode_root_element;
+    private List<ProtocolEncodeInternalMethod> encode_elements;
     private List<JavaModelWritable> common_methods;
     private Set<String> autowires;
 
@@ -38,17 +40,20 @@ public class ProtocolImpl extends JavaModelWritable {
 
     public Set<String> getImport_stats() {
         if (autowires.size() != 0) {
-            import_stats.add("org.springframework.beans.factory.annotation.Autowired");
+            super.import_stats.add("org.springframework.beans.factory.annotation.Autowired");
         }
+        super.import_stats.add(CHANNEL_UTILS_QUALIFIER_NAME);
+        // 移除null值
+        super.import_stats.remove(null);
         return import_stats;
     }
 
-    public void setDecode_root_element(ProtocolDecode$0 decode_root_element) {
+    public void setDecode_root_element(ProtocolOverrideDecodeMethod decode_root_element) {
         this.decode_root_element = decode_root_element;
         super.import_stats.addAll(decode_root_element.getImport_stats());
     }
 
-    public void setEncode_root_element(ProtocolEncode$0 encode_root_element) {
+    public void setEncode_root_element(ProtocolOverrideEncodeMethod encode_root_element) {
         this.encode_root_element = encode_root_element;
         super.import_stats.addAll(encode_root_element.getImport_stats());
     }
@@ -61,12 +66,12 @@ public class ProtocolImpl extends JavaModelWritable {
         autowires.add(autowireName);
     }
 
-    public synchronized void addDecodeElement(ProtocolDecode$1 ele) {
+    public synchronized void addDecodeElement(ProtocolDecodeInternalMethod ele) {
         decode_elements.add(ele);
         import_stats.addAll(ele.getImport_stats());
     }
 
-    public synchronized void addEncodeElement(ProtocolEncode$1 ele) {
+    public synchronized void addEncodeElement(ProtocolEncodeInternalMethod ele) {
         encode_elements.add(ele);
         import_stats.addAll(ele.getImport_stats());
     }
